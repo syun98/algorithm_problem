0547ef7503632c141d237090ba564c5b8792c3cb
chocoo98 <chocoo98@naver.com>
2019-12-29 03:58:26 +0900

    Create 1316.cpp

---
+++ 1316.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	int n;
+	scanf("%d", &n);
+	string str;
+
+	int cnt = n;
+	for (int k = 0; k < n; k++) {
+		cin >> str;
+		for (int i = 0; i < str.length(); i++) {
+			if (str[i] != str[i + 1]) {
+				for (int j = i + 1; j < str.length(); j++) {
+					if (str[i] == str[j]) {
+						cnt--;
+						goto STOPCNT;
+					}
+				}
+			}		
+		}
+	STOPCNT:;
+	}
+	printf("%d", cnt);
+}
