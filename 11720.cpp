3acf4b0d80e4abaf2a109586c368639b92612e6e
chocoo98 <chocoo98@naver.com>
2019-12-29 04:21:03 +0900

    Create 11720.cpp

---
+++ 11720.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+
+int main() {
+	int N, result = 0;
+	int num[100];
+
+	scanf("%d", &N);
+	for (int i = 0; i < N; i++) {
+		scanf("%1d", &num[i]);
+	}
+
+	for(int i=0;i<N;i++){
+		result += num[i];
+	}
+	printf("%d\n", result);
+}
