b681c75758a6529d2570bd06430aca080e816027
chocoo98 <chocoo98@naver.com>
2019-12-29 04:21:43 +0900

    Create 10809.cpp

---
+++ 10809.cpp
@@ -0,0 +1,25 @@
+#include <cstdio>
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main() {
+	string str;
+	getline(cin, str, '\n');
+	int len, n = 0;
+	len = str.length();
+
+	for (int i = 'a'; i <= 'z'; i++) {
+		for (int j = 0; j < len; j++) {
+			if (str.at(j) == i) {
+				printf("%d ", j);
+				n++;
+				break;
+			}			
+		}
+		if (n == 0) {
+			printf("-1 ");
+		}
+		n = 0;
+	}
+}
