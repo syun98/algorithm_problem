2424a1ad554d17974a9f7a817b55baf56c4fe1f8
chocoo98 <chocoo98@naver.com>
2019-12-29 04:20:19 +0900

    Create 2908.cpp

---
+++ 2908.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	int a, b;
+	scanf("%d %d", &a, &b);
+
+	int reva = 0, revb = 0;
+	reva += (a % 10)*100;
+	reva += (a % 100 - a % 10);
+	reva += a / 100;
+
+	revb += (b % 10) * 100;
+	revb += (b % 100 - b % 10);
+	revb += b / 100;
+
+	if (reva > revb)
+		printf("%d", reva);
+	else
+		printf("%d", revb);
+}
