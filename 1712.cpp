4ea7fb915c88c6083def7692f7f8da1cdd7ffaba
chocoo98 <chocoo98@naver.com>
2020-01-01 00:06:52 +0900

    Create 1712.cpp

---
+++ 1712.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main() {
+	int A, B, C;
+	scanf_s("%d %d %d", &A, &B, &C);
+	if (B >= C) {
+		printf("-1");
+		return 0;
+	}
+	else {
+		printf("%d", A / (C - B) + 1);
+	}
+	return 0;
+}
