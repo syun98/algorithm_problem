9505e3829e43ba7361209cb0d04f3d385fe61efb
chocoo98 <chocoo98@naver.com>
2020-04-12 00:26:56 +0900

    Create 8958.cpp

---
+++ 8958.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	int n;
+	string str;
+	int count = 0;
+	int result = 0;
+	
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> str;
+
+		if (str.at(0) == 'O') {
+			result++;
+			count++;
+		}
+
+		for (int j = 1; j < str.size(); j++) {
+			if (str.at(j) == 'O') {
+				if (str.at(j) == str.at(j - 1)) {
+					count++;
+					result += count;
+				}
+				else {
+					result++;
+					count++;
+				}
+			}
+			else {
+				count = 0;
+			}
+
+		}
+		cout << result << endl;
+		result = 0;
+		count = 0;
+	}
+}
