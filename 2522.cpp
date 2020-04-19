49d797c99947b41f681cd09613d565e4e36b74f8
chocoo98 <chocoo98@naver.com>
2020-04-20 01:57:11 +0900

    Create 2522.cpp

---
+++ 2522.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (j < (n - i - 1)) {
+				cout << ' ';
+			}
+			else {
+				cout << '*';
+			}
+		}
+		cout << endl;
+	}
+	
+	for (int i = (n - 2); i >= 0; i--) {
+		for (int j = 0; j < n; j++) {
+			if (j < (n - i - 1)) {
+				cout << ' ';
+			}
+			else {
+				cout << '*';
+			}
+		}
+		cout << endl;
+	}
+}
