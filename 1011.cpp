19f832efc62aac7476d252df2feb57deb7e4e9ef
chocoo98 <chocoo98@naver.com>
2020-05-15 23:20:15 +0900

    Update 1011.cpp
    modify because of time out error

--- 1011.cpp
+++ 1011.cpp
@@ -1,38 +1,24 @@
 #include <iostream>
+#include <cmath>
+
 using namespace std;
-
-int AlpaCentauri(int distance) {
-
-	if (distance == 1)
-		return 1;
-	else if (distance == 2)
-		return 2;
-
-	int i = 2;	//i
-	int cnt = 0;	//cnt
-	int j = 2;		//j
-
-
-	while (1) {
-		if (cnt == 2) {
-			i++;
-			cnt = 0;
-		}
-		j += i;
-		cnt++;
-		if (j >= distance)
-			break;
-	}
-	return (cnt == 1) ? i * 2 - 1 : i * 2;
-}
 
 int main() {
 	int t;
-	int x, y, result;
-	scanf_s("%d", &t);
+	cin >> t;
+	for (int j = 0; j < t; j++) {
+		long double x, y, d, i = 1;
+		cin >> x >> y;
+		d = y - x;
+		for (;; i++)
+			if (i*i > d)
+				break;
 
-	for (int i = 0; i < t; i++) {
-		scanf_s("%d %d", &x, &y);
-		printf("%d\n", AlpaCentauri(y - x));
+		if (pow(i - 1, 2) == y - x)
+			cout << 2 * i - 3 << '\n';
+		else if ((pow(i, 2) + pow(i - 1, 2)) / 2 >= y - x)
+			cout << 2 * (i - 1) << '\n';
+		else
+			cout << 2 * (i - 1) + 1 << '\n';
 	}
 }
