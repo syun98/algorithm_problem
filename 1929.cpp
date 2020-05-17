0deb708ebf53db7e45feb7f002e56e34ebf2250c
chocoo98 <chocoo98@naver.com>
2020-05-18 00:28:56 +0900

    Update 1929.cpp

--- 1929.cpp
+++ 1929.cpp
@@ -1,23 +1,41 @@
 #include <iostream>
+#include <cmath>
+#include <cstdio>
 using namespace std;
 
 int main() {
 	int m, n;
-	int temp = 0;
+	scanf_s("%d %d", &m, &n);
 
-	cin >> m >> n;
+	bool * arr = new bool[n + 1];
 
-	for (int i = m; i <= n; i++) {
-
-		for (int j = 1; j <= i; j++) {
-			if (i % j == 0)
-				temp++;
-		}
-
-		if (temp == 2) {
-			printf("%d\n", i);
-		}
-
-		temp = 0;
+	for (int i = 0; i < n + 1; i++) {
+		arr[i] = true;
 	}
+
+	int j;
+
+	for (int i = 2; i < n + 1; i++) {
+		if (arr[i]) {
+			if ((unsigned int)pow(i, 2) > 1000001) {
+				break;
+			}
+			else {
+				for (j = (int)pow(i, 2); j < n + 1;) {
+					arr[j] = false;
+					j += i;
+				}
+			}
+		}
+	}
+
+	if (m == 1)
+		m++;
+
+	for (int i = m; i < n + 1; i++) {
+		if (arr[i] && i >= m)
+			printf("%d\n", i);
+	}
+
+	delete[]arr;
 }
