86865c25b6d591cd774718ae5cdb52a4ddef5e86
chocoo98 <chocoo98@naver.com>
2020-05-25 00:15:12 +0900

    Update 1002.cpp

--- 1002.cpp
+++ 1002.cpp
@@ -12,18 +12,24 @@ int main() {
 	for (int i = 0; i < t; i++) {
 		cin >> x1 >> y1 >> r1 >> x2 >> y2 >> r2;
 
-		d = sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2));
+		d = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+		int minus = abs(r1 - r2);
+		int plus = r1 + r2;
 
-		if (x1 == x2 && y1 == y2 && r1 == r2)
-			result = -1;
-		else if (x1 == x2 && y1 == y2 && r1 != r2)
-			result = 0;
-		else if (r1 + r2 > d)
-			result = 2;
-		else if (r1 + r2 == d)
-			result = 1;
-		else if (r1 + r2 < d)
-			result = 0;
+		if (d == 0) {
+			if (r1 == r2)
+				result = -1;
+			else
+				result = 0;
+		}
+		else {
+			if (minus<d&&plus>d)
+				result = 2;
+			else if (plus == d || minus == d)
+				result = 1;
+			else
+				result = 0;
+		}
 
 		cout << result << endl;
 	}
