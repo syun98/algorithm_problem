a04579bedbce064545beb097ec5b2cfad632df0c
chocoo98 <chocoo98@naver.com>
2020-06-02 00:47:59 +0900

    Update 2108.cpp

--- 2108.cpp
+++ 2108.cpp
@@ -7,13 +7,23 @@ int main() {
 	int n;
 	double avg = 0;
 	int median, mode = 0, range;
-	int arr[500];	//500000
+	int cnt[8001] = {}, max = 0;
+	int arr[500000];
 	cin >> n;
 
+	int temp = 0;
 	for (int i = 0; i < n; i++) {
 		cin >> arr[i];
 		avg += arr[i];
+		if (arr[i] < 0)
+			temp = arr[i] + 8001;
+		else
+			temp = arr[i];
+		cnt[temp]++;
+		if (cnt[temp] > max)
+			max = cnt[temp];
 	}
+
 	sort(arr, arr + n);
 
 	avg = round(avg / n);
@@ -23,10 +33,26 @@ int main() {
 	else
 		median = arr[n / 2 - 1];
 
-	/*mode ±¸ÇÏ±â*/
+	int max_cnt = 0;
+	for (int i = 4001; i < 8001; i++) {
+		if (cnt[i] == max) {
+			if (max_cnt < 2)
+				mode = i;
+			max_cnt++;
+		}
+	}
+	for (int i = 0; i < 4001; i++) {
+		if (cnt[i] == max) {
+			if (max_cnt < 2)
+				mode = i;
+			max_cnt++;
+		}
+	}
+
+	if (mode > 4000)
+		mode -= 8001;
 
 	range = arr[n - 1] - arr[0];
 
 	cout << avg << endl << median << endl << mode << endl << range << endl;
-	
 }
