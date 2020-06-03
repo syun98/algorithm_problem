e0cb3371d4791f359c85f9a77124b8857807f24e
chocoo98 <chocoo98@naver.com>
2020-06-04 00:48:14 +0900

    Update 11650.cpp

--- 11650.cpp
+++ 11650.cpp
@@ -1,18 +1,21 @@
 #include <iostream>
+#include <vector>
 #include <algorithm>
 using namespace std;
 
-int main() {
+int main(void) {
 	int n;
-	int x[100000][2] = {};
 	cin >> n;
-	
-	for (int i = 0; i < n; i++) 
-		cin >> x[i][0] >> x[i][1];
-	
-	//error
-	sort(x[0][0], x[n][0]);
+	vector<vector<int>> x(n, vector<int>(2, 0));
 
-	for (int i = 0; i < n; i++)
-		cout << x[i][0] << x[i][1] << endl;
+	for (int i = 0; i < n; i++) {
+		cin >> x[i][0] >> x[i][1];
+	}
+
+	sort(x.begin(), x.end());
+
+	for (int i = 0; i < x.size(); i++) {
+		cout << x[i][0] << " " << x[i][1] << '\n';
+	}
+
 }
