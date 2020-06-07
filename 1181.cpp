e978dadd9cdca07e08c18754dc1fe9c91b796394
chocoo98 <chocoo98@naver.com>
2020-06-08 05:51:16 +0900

    Create 1181.cpp

---
+++ 1181.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+bool compareStr(string a, string b) {
+	if (a.length() == b.length())
+		return a < b;
+	return a.length() < b.length();
+}
+
+int main(void) {
+	int n;
+	vector<string> vec;
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		string temp;
+		cin >> temp;
+		if (find(vec.begin(), vec.end(), temp) == vec.end())
+			vec.push_back(temp);
+	}
+	sort(vec.begin(), vec.end(), compareStr);
+
+	for (int i = 0; i < vec.size(); i++) {
+		cout << vec[i] << endl;
+	}
+}
