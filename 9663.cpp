1a773a65a2ac903fb5fbd14452a3f2f5ceff7e10
chocoo98 <chocoo98@naver.com>
2020-06-14 23:41:26 +0900

    Update 9663.cpp

--- 9663.cpp
+++ 9663.cpp
@@ -6,7 +6,8 @@ int res;
 int board[15] = {};
 
 bool chk(int row) {
-	for (int i = 0; i < row; i++) {
+	for (int i = 0; i < row; i++) {	//i는 0~마지막 여왕 바로 윗 행까지
+		//같은 열에 있거나 대각선에 있다면
 		if (board[row] == board[i] || abs(row - i) == abs(board[row] - board[i]))
 			return false;
 	}
@@ -19,10 +20,12 @@ void run(int row) {
 		return;
 	}
 
-	for (int i = 0; i < n; i++) {
+	//첫 번째 행에 놓이는 말의 위치를 i가 정함
+	for (int i = 0; i < n; i++) {	//i가 열의 위치
 		board[row] = i;
-		if (chk(row))
+		if (chk(row))		//chk한게 통과했으면
 			run(row + 1);
+		//통과 끝나면
 		board[row] = 0;
 	}
 }
