756ead0a8fed0ed60b47eb2310e7b29e127305a4
chocoo98 <chocoo98@naver.com>
2020-06-16 01:59:09 +0900

    Update 2580.cpp

--- 2580.cpp
+++ 2580.cpp
@@ -1,32 +1,31 @@
 #include <iostream>
 using namespace std;
 
-int a[81] = {};
+int a[9][9] = {};
 
-int sdoku(int i) {
-
+void sdoku(int i, int j) {
+	int res = 45;
 }
 
 int main(void) {
-	for (int i = 0; i < 81; i++)
-		cin >> a[i];
-	
-	int cnt = 0;
-	bool chk = true;
-	while (1) {
-		for (int i = 0; i < 81; i++) {
-			if (a[i] == 0) {
-				a[i] = sdoku(i);
-				chk = false;
-			}
-
+	for (int i = 0; i < 9; i++) {
+		for (int j = 0; j < 9; j++) {
+			cin >> a[i][j];
 		}
 	}
 
+	for (int i = 0; i < 9; i++) {
+		for (int j = 0; j < 9; j++) {
+			if (a[i][j] == 0)
+				sdoku(i, j);
+		}
+	}
+	cout << endl;	//»ý·«
 
-	for (int i = 0; i < 81; i++) {
-		if (i % 9 == 0 && i != 0)
-			cout << '\n';
-		cout << a[i]<<' ';
+	for (int i = 0; i < 9; i++) {
+		for (int j = 0; j < 9; j++) {
+			cout << a[i][j] << ' ';
+		}
+		cout << '\n';
 	}
 }
