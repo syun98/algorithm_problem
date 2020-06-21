b3017e72b720d4e41cb00640d863f5b084f82f1c
chocoo98 <chocoo98@naver.com>
2020-06-22 01:54:38 +0900

    Update 2580.cpp
    time out

--- 2580.cpp
+++ 2580.cpp
@@ -2,13 +2,80 @@
 using namespace std;
 
 int arr[9][9] = {};
+int res_cnt = 81;
 
 void sdoku(int i, int j) {
+	int chk[9] = { 0, };
+	int zeroChk = 0;
 
+	for (int k = 0; k < 9; k++) {
+		if (arr[i][k] == 0) {
+			zeroChk++;
+		}
+		else {
+			chk[arr[i][k] - 1]++;
+		}
+	}
+
+	if (zeroChk == 1) {
+		for (int k = 0; k < 9; k++) {
+			if (chk[k] == 0) {
+				arr[i][j] = k + 1;
+				res_cnt--;
+				return;
+			}
+			chk[k] = 0;
+		}
+	}
+
+	zeroChk = 0;
+
+	for (int k = 0; k < 9; k++) {
+		if (arr[k][j] == 0) {
+			zeroChk++;
+		}
+		else {
+			chk[arr[k][j] - 1]++;
+		}
+	}
+
+	if (zeroChk == 1) {
+		for (int k = 0; k < 9; k++) {
+			if (chk[k] == 0) {
+				arr[i][j] = k + 1;
+				res_cnt--;
+				return;
+			}
+			chk[k] = 0;
+		}
+	}
+
+	zeroChk = 0;
+
+	for (int k = i / 3 * 3; k < i / 3 * 3 + 3; k++) {
+		for (int L = j / 3 * 3; L < j / 3 * 3 + 3; L++) {
+			if (arr[L][k] == 0) {
+				zeroChk++;
+			}
+			else {
+				chk[arr[L][k] - 1]++;
+			}
+		}
+	}
+
+	if (zeroChk == 1) {
+		for (int k = 0; k < 9; k++) {
+			if (chk[k] == 0) {
+				arr[i][j] = k + 1;
+				res_cnt--;
+				return;
+			}
+			chk[k] = 0;
+		}
+	}
 }
 
-int main(void) {
-	int res_cnt = 81;
+int main() {
 	for (int i = 0; i < 9; i++) {
 		for (int j = 0; j < 9; j++) {
 			cin >> arr[i][j];
@@ -16,17 +83,19 @@ int main(void) {
 	}
 
 again:;
-
+	res_cnt = 81;
 	for (int i = 0; i < 9; i++) {
 		for (int j = 0; j < 9; j++) {
 			if (arr[i][j] == 0)
 				sdoku(i, j);
+			else
+				res_cnt--;
 		}
 	}
 
 	if (res_cnt)
 		goto again;
-	cout << endl;	//»ı·«
+	//cout << endl;
 
 	for (int i = 0; i < 9; i++) {
 		for (int j = 0; j < 9; j++) {
