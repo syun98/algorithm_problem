845ee001c9d773245d79bab52a652169bfea739a
chocoo98 <chocoo98@naver.com>
2020-06-23 04:34:29 +0900

    Create 14889.cpp

---
+++ 14889.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int n;
+int arr[20][20] = {};
+
+int main() {
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cin >> arr[i][j];
+		}
+	}
+}
