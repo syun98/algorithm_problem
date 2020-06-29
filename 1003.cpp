7d15779aa4dbdba6859071c16a580af919e3c623
chocoo98 <chocoo98@naver.com>
2020-06-30 00:10:13 +0900

    Update 1003.cpp

--- 1003.cpp
+++ 1003.cpp
@@ -1,27 +1,23 @@
 #include <iostream>
 using namespace std;
 
-int cnt[2] = {};
-
-void fibonacci(int n) {
-	if (n == 0)
-		cnt[0]++;
-	else if (n == 1)
-		cnt[1]++;
-	else {
-		fibonacci(n - 1);
-		fibonacci(n - 2);
-	}
-}
-
 int main() {
 	int t;
-	scanf("%d", &t);
+	scanf_s("%d", &t);
+
+	int arr[41] = { 0,1,1 };
+	for (int i = 3; i < 41; i++)
+		arr[i] = arr[i - 1] + arr[i - 2];
+
 	for (int i = 0; i < t; i++) {
 		int n;
-		scanf("%d", &n);
-		fibonacci(n);
-		printf("%d %d\n", cnt[0], cnt[1]);
-		cnt[0] = cnt[1] = 0;
+		scanf_s("%d", &n);
+
+		if (n == 0)
+			printf("1 0\n");
+		else if (n == 1)
+			printf("0 1\n");
+		else
+			printf("%d %d\n", arr[n - 1], arr[n]);
 	}
 }
