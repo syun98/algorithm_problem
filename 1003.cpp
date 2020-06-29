9331bbb7031efaf2b7acad45e6260391f0360af0
chocoo98 <chocoo98@naver.com>
2020-06-30 00:08:55 +0900

    Update 1003.cpp

--- 1003.cpp
+++ 1003.cpp
@@ -1,18 +1,28 @@
 #include <iostream>
 using namespace std;
 
+int cnt[2] = {};
+
+void fibonacci(int n) {
+	if (n == 0)
+		cnt[0]++;
+	else if (n == 1)
+		cnt[1]++;
+	else {
+		fibonacci(n - 1);
+		fibonacci(n - 2);
+	}
+}
+
 int main() {
 	int t;
 	cin >> t;
 	for (int i = 0; i < t; i++) {
 		int n;
-		long long fibo[91] = { 0,1, };
-		scanf_s("%d", &n);
+		cin >> n;
 
-		for (int i = 2; i <= n; i++) {
-			fibo[i] = fibo[i - 1] + fibo[i - 2];
-		}
-
-		cout << fibo[n];
+		fibonacci(n);
+		cout << cnt[0] << " " << cnt[1] << endl;
+		cnt[0] = cnt[1] = 0;
 	}
 }
