14ebead176b66fc021837b2eb0eef3036c398843
chocoo98 <chocoo98@naver.com>
2020-07-04 00:53:44 +0900

    Update 1149.cpp

--- 1149.cpp
+++ 1149.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
 using namespace std;
 
+int arr[1000][3] = { 0, };
+
+void Solve(int n) {
+	int result = -1;
+
+	cout << result << endl;
+}
+
 int main() {
 	int n;
-	int arr[1000][3] = { 0, };
+
 	cin >> n;
 
 	for (int i = 0; i < n; i++) {
 		cin >> arr[i][0] >> arr[i][1] >> arr[i][2];
 	}
+
+	Solve(n);
 }
