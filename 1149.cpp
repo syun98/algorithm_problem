02eaf2edde642e9cb93f8812f711dd75babdc3f4
chocoo98 <chocoo98@naver.com>
2020-07-06 04:31:57 +0900

    Update 1149.cpp

--- 1149.cpp
+++ 1149.cpp
@@ -4,8 +4,24 @@ using namespace std;
 int arr[1000][3] = { 0, };
 bool chk[1000][3];
 
-void Solve(int n) {
-	int result = -1;
+void Solve(int n, int cnt) {
+	int result = 3001;
+	int temp = 3001;
+
+	if (chk[n][cnt])
+		return;
+
+	if (cnt == 3) {
+		if (temp < result)
+			result = temp;
+		temp = 0;
+	}
+	else {
+		temp += arr[n][cnt];
+		chk[n][cnt] = true;
+		Solve(n - 1, cnt + 1);
+		chk[n][cnt] = false;
+	}
 
 	cout << result << endl;
 }
@@ -19,5 +35,5 @@ int main() {
 		cin >> arr[i][0] >> arr[i][1] >> arr[i][2];
 	}
 
-	Solve(n);
+	Solve(n, 0);
 }
