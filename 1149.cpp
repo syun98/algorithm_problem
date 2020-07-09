8d7c9fada629abb96077305dec5583b9ea55dcc3
chocoo98 <chocoo98@naver.com>
2020-07-09 23:43:50 +0900

    Update 1149.cpp

--- 1149.cpp
+++ 1149.cpp
@@ -1,39 +1,23 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
-int arr[1000][3] = { 0, };
-bool chk[1000][3];
-int result = 3001;
-
-void Solve(int n, int cnt) {
-	int temp = 3001;
-
-	if (chk[n][cnt])
-		return;
-
-	if (cnt == 3) {
-		if (temp < result)
-			result = temp;
-		temp = 0;
-	}
-	else {
-		temp += arr[n][cnt];
-		chk[n][cnt] = true;
-		Solve(n - 1, cnt + 1);
-		chk[n][cnt] = false;
-	}
-
-}
-
 int main() {
+	int arr[1000][3] = { 0, };
+	int result[1000][3] = { 0, };
 	int n;
+
 	cin >> n;
 
-	for (int i = 0; i < n; i++) {
+	for (int i = 1; i <= n; i++) {
 		cin >> arr[i][0] >> arr[i][1] >> arr[i][2];
 	}
 
-	Solve(n,0);
+	for (int i = 1; i <= n; i++) {
+		result[i][0] = arr[i][0] + min(result[i - 1][1], result[i - 1][2]);
+		result[i][1] = arr[i][1] + min(result[i - 1][0], result[i - 1][2]);
+		result[i][2] = arr[i][2] + min(result[i - 1][1], result[i - 1][0]);
+	}
 
-	cout << result << endl;
+	cout << min(result[n][0], min(result[n][1], result[n][2]));
 }
