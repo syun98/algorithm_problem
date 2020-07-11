7430f50ef214396cb93397322939cbbcd3b6cbfa
chocoo98 <chocoo98@naver.com>
2020-07-11 17:25:03 +0900

    Update 1932.cpp

--- 1932.cpp
+++ 1932.cpp
@@ -4,20 +4,24 @@ using namespace std;
 
 int main() {
 	int n;
-	int arr[500][500] = {};
+	int arr[501][501] = {};
 	int result = 0;
 
 	cin >> n;
 
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < i + 1; j++)
+	for (int i = 1; i <= n; i++)
+		for (int j = 1; j <= i; j++)
 			cin >> arr[i][j];
 
-	result += arr[0][0];
-	int temp = 0;
-	for (int i = 1; i < n; i++) {
-		temp = arr[i][temp] > arr[i][temp + 1] ? temp : temp + 1;
-		result += arr[i][temp];
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= i; j++) {
+			arr[i][j] += max(arr[i - 1][j], arr[i - 1][j - 1]);
+		}
+	}
+
+	for (int i = 1; i <= n; i++) {
+		if (result < arr[n][i])
+			result = arr[n][i];
 	}
 
 	cout << result << endl;
