be30efd36daa563a82400020d21270ba8435fe09
chocoo98 <chocoo98@naver.com>
2020-07-13 23:50:18 +0900

    Update 2579.cpp

--- 2579.cpp
+++ 2579.cpp
@@ -4,13 +4,20 @@ using namespace std;
 
 int main() {
 	int n;
-	int stair[301] = {};
-	int result = 0;
+	int arr[301] = {};
+	int result[301] = {};
 
 	cin >> n;
 
 	for (int i = 1; i <= n; i++)
-			cin >> stair[i];
+		cin >> arr[i];
 
-	cout << result << endl;
+	result[1] = arr[1];
+	result[2] = arr[1] + arr[2];
+	result[3] = max(arr[1] + arr[3], arr[2] + arr[3]);
+
+	for (int i = 4; i <= n; i++)
+		result[i] = max(result[i - 2] + arr[i], result[i - 3] + arr[i - 1] + arr[i]);
+
+	cout << result[n] << endl;
 }
