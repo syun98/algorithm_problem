fe297fa510802fb541d7cf86fcfcfa43da403403
chocoo98 <chocoo98@naver.com>
2020-09-22 03:47:05 +0900

    Update 1001.cpp

--- 1001.cpp
+++ 1001.cpp
@@ -1,8 +1,9 @@
+//blog : https://codesyun.tistory.com/8
 #include <iostream>
 using namespace std;
 
-int main(){
-    int a, b;
-    cin >> a >> b;
-    cout << (a - b) << endl;
+int main() {
+	int a, b;
+	cin >> a >> b;
+	cout << (a - b) << endl;
 }
