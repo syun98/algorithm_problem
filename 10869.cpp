3802dc7b881e484383b56caa8241c8f72abfc1e7
chocoo98 <chocoo98@naver.com>
2020-09-25 05:21:19 +0900

    Create 10869.cpp

---
+++ 10869.cpp
@@ -0,0 +1,10 @@
+//blog : https://codesyun.tistory.com/11
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+
+	cout << a + b << endl << a - b << endl << a * b << endl << a / b << endl << a % b << endl;
+}
