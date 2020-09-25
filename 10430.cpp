f9017ef19f3b11b81bde999b6311145b625cc128
chocoo98 <chocoo98@naver.com>
2020-09-26 00:09:07 +0900

    Create 10430.cpp

---
+++ 10430.cpp
@@ -0,0 +1,11 @@
+//blog : https://codesyun.tistory.com/12
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b, c;
+	cin >> a >> b >> c;
+
+	cout << (a + b) % c << endl << ((a%c) + (b%c)) % c << endl
+		<< (a*b) % c << endl << ((a%c)*(b%c)) % c << endl;
+}
