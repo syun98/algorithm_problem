12079646108dcb96dfade55c3b95ef564433ed0f
Sihyun Lee <chocoo98@naver.com>
2020-10-07 23:46:10 +0900

    Create 11021.cpp

---
+++ 11021.cpp
@@ -0,0 +1,13 @@
+//blog : https://codesyun.tistory.com/25
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	int a, b;
+	cin >> t;
+	for (int i = 1; i <= t; i++) {
+		cin >> a >> b;
+		cout << "Case #" << i << ": " << a + b << '\n';
+	}
+}
