0fa434ded8579f55b6d5c140367520455e630ec9
chocoo98 <chocoo98@naver.com>
2020-10-08 01:55:19 +0900

    Create 11022.cpp

---
+++ 11022.cpp
@@ -0,0 +1,14 @@
+//blog : https://codesyun.tistory.com/26
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	int a, b;
+	cin >> t;
+	for (int i = 1; i <= t; i++) {
+		cin >> a >> b;
+		cout << "Case #" << i << ": " << a << " + " << b << " = "
+			<< a + b << '\n';
+	}
+}
