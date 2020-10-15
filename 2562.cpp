8746ca674cefd9321d9c302f21add2dd79564cb9
chocoo98 <chocoo98@naver.com>
2020-10-16 00:03:01 +0900

    Update 2562.cpp

--- 2562.cpp
+++ 2562.cpp
@@ -1,22 +1,16 @@
+//blog : https://codesyun.tistory.com/34
 #include <iostream>
-
 using namespace std;
 
 int main() {
 	int num[9];
-
-	for (int i = 0; i < 9; i++) {
-		cin >> num[i];
-	}
-
 	int maxvalue = 0, maxindex = 0;
 	for (int i = 0; i < 9; i++) {
+		cin >> num[i];
 		if (maxvalue < num[i]) {
 			maxvalue = num[i];
 			maxindex = i;
 		}
 	}
-
 	cout << maxvalue << endl << maxindex + 1;
-
 }
