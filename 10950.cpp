7851f7f5387e057253c82af83ec058f234973a70
chocoo98 <chocoo98@naver.com>
2020-10-23 05:16:20 +0900

    Create 10950.cpp

---
+++ 10950.cpp
@@ -0,0 +1,13 @@
+//blog : https://codesyun.tistory.com/20
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	int a, b;
+	cin >> t;
+	for (int i = 0; i < t; i++) {
+		cin >> a >> b;
+		cout << a + b << endl;
+	}
+}
