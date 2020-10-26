03f5f6768a637afc6814706e875a72a031353453
chocoo98 <chocoo98@naver.com>
2020-10-27 00:49:36 +0900

    Create 2884.cpp

---
+++ 2884.cpp
@@ -0,0 +1,18 @@
+//blog : https://codesyun.tistory.com/18
+#include <iostream>
+using namespace std;
+
+int main() {
+	int hour, min;
+	cin >> hour >> min;
+
+	if (min < 45) {
+		if (hour == 0)
+			cout << 23 << " " << min + 15 << endl;
+		else
+			cout << hour - 1 << " " << min + 15 << endl;
+	}
+	else {
+		cout << hour << " " << min - 45 << endl;
+	}
+}
