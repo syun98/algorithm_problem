0fa1fb5fef412b8937da874e006946bf476e7fed
chocoo98 <chocoo98@naver.com>
2020-11-11 00:13:56 +0900

    Update 1011.cpp

--- 1011.cpp
+++ 1011.cpp
@@ -1,24 +1,32 @@
+//blog : https://codesyun.tistory.com/62
 #include <iostream>
-#include <cmath>
-
 using namespace std;
 
 int main() {
-	int t;
-	cin >> t;
-	for (int j = 0; j < t; j++) {
-		long double x, y, d, i = 1;
+	int T;
+	cin >> T;
+	for (int test_case = 0; test_case < T; test_case++) {
+		double x, y, result;
 		cin >> x >> y;
-		d = y - x;
-		for (;; i++)
-			if (i*i > d)
-				break;
 
-		if (pow(i - 1, 2) == y - x)
-			cout << 2 * i - 3 << '\n';
-		else if ((pow(i, 2) + pow(i - 1, 2)) / 2 >= y - x)
-			cout << 2 * (i - 1) << '\n';
-		else
-			cout << 2 * (i - 1) + 1 << '\n';
+		double i = 1;
+		for (;; i++) {
+			if (y - x < i*i)
+				break;
+		}
+
+		//거리가 i-1의 제곱수일때
+		if (y - x == (i - 1)*(i - 1)) {
+			result = 2 * (i - 1) - 1;
+		}
+		//거리가 i-1의 제곱수와 중간값 사이에 있을 때
+		else if (y - x < ((i - 1)*(i - 1) + i * i) / 2) {
+			result = 2 * (i - 1);
+		}
+		//거리가 중간값과 i의 제곱수 사이에 있을 때
+		else {
+			result = 2 * i - 1;
+		}
+		cout << result << '\n';
 	}
 }
