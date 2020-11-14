29c7d40f7d6eb1fb8d648de7c1d833eb3fd32667
chocoo98 <chocoo98@naver.com>
2020-11-15 03:25:24 +0900

    Update 4948.cpp

--- 4948.cpp
+++ 4948.cpp
@@ -1,47 +1,33 @@
+//blog : https://codesyun.tistory.com/66
 #include <iostream>
 #include <cmath>
-#include <cstdio>
 using namespace std;
 
 int main() {
-	int n = 1;
-	int cnt = 0;
+	int n, rt, cnt = 0;
 
-	while (n != 0) {
+	while (1) {
 		cin >> n;
-
-		if (n == 0)
+		if (!n)	//0 입력시 종료
 			break;
 
-		//에라토스테네스의 체 사용
-		bool * arr = new bool[2 * n + 1];
-
-		for (int i = 0; i < 2 * n + 1; i++) {
-			arr[i] = true;
-		}
-
-		int j;
-
-		for (int i = 2; i < 2 * n + 1; i++) {
-			if (arr[i]) {
-				if ((unsigned int)pow(i, 2) > 1000001) {
-					break;
-				}
-				else {
-					for (j = (int)pow(i, 2); j < 2 * n + 1;) {
-						arr[j] = false;
-						j += i;
+		for (int i = n + 1; i <= 2 * n; i++) {
+			rt = sqrt(i);
+			if (rt == 1 && i != 1) {	//2,3인 경우
+				cnt++;
+				continue;
+			}
+			if (i % 2) {	//홀수일 경우
+				for (int j = 2; j <= rt; j++) {
+					if (!(i%j))
+						break;
+					if (j == rt) {
+						cnt++;
 					}
 				}
 			}
 		}
-
-		for (int i = n + 1; i < 2 * n + 1; i++) {
-			if (arr[i] && i >= n)
-				cnt++;
-		}
-		cout << cnt << endl;
+		cout << cnt << '\n';
 		cnt = 0;
-		delete[]arr;
 	}
 }
