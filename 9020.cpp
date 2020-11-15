33e9cc33c45219ff7e6dd3cab61fff39d003f892
chocoo98 <chocoo98@naver.com>
2020-11-16 03:16:21 +0900

    Update 9020.cpp

--- 9020.cpp
+++ 9020.cpp
@@ -1,37 +1,36 @@
+//blog : https://codesyun.tistory.com/67
 #include <iostream>
 #include <cmath>
 using namespace std;
 
-bool isPrime(int n) {
-	int root = sqrt(n);
-	if (root == 1)
+bool isPrime(int i) {
+	int rt;
+	rt = sqrt(i);
+	if (rt == 1 && i != 1) {	//2,3인 경우
 		return true;
-	if (n % 2) {
-		for (int i = 2; i <= root; i++) {
-			if (!(n%i))
+	}
+	if (i % 2) {	//홀수일 경우
+		for (int j = 2; j <= rt; j++) {
+			if (!(i%j))
 				return false;
-			if (i == root)
+			if (j == rt) {
 				return true;
+			}
 		}
 	}
-	return false;
 }
 
 int main() {
-	int t;
-	int n;
-	cin >> t;
-	
-	int a, b;
+	int T;
+	cin >> T;
 
-	for (int i = 0; i < t; i++) {
+	for (int test_case = 0; test_case < T; test_case++) {
+		int n;
 		cin >> n;
 
-		for (int j = n / 2; j >= 2; j--) {
-			a = j;
-			b = n - a;
-			if (isPrime(a) && isPrime(b)) {
-				cout << a << " " << b << endl;
+		for (int i = n / 2; i >= 2; i--) {
+			if (isPrime(i) && isPrime(n - i)) {
+				cout << i << " " << n - i << '\n';
 				break;
 			}
 		}
