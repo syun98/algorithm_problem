4b88dd6930d661427fa3a8fc61c0d8eb4d73439e
chocoo98 <chocoo98@naver.com>
2020-11-21 04:02:38 +0900

    Update 1002.cpp

--- 1002.cpp
+++ 1002.cpp
@@ -1,36 +1,30 @@
+//blog : https://codesyun.tistory.com/72
 #include <iostream>
-#include <cmath>
 using namespace std;
 
 int main() {
-	int t;
-	int x1, x2, y1, y2, r1, r2;
-	double d;
-	int result = 0;
-	cin >> t;
+	int T;
+	int x1, y1, r1, x2, y2, r2;
+	int d, cond1, cond2;
+	cin >> T;
 
-	for (int i = 0; i < t; i++) {
+	for (int test = 0; test < T; test++) {
 		cin >> x1 >> y1 >> r1 >> x2 >> y2 >> r2;
-
-		d = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
-		int minus = abs(r1 - r2);
-		int plus = r1 + r2;
+		d = (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
+		cond1 = (r1 - r2) * (r1 - r2);
+		cond2 = (r1 + r2) * (r1 + r2);
 
 		if (d == 0) {
-			if (r1 == r2)
-				result = -1;
+			if (cond1 == 0)
+				cout << "-1" << '\n';
 			else
-				result = 0;
+				cout << "0" << '\n';
 		}
-		else {
-			if (minus<d&&plus>d)
-				result = 2;
-			else if (plus == d || minus == d)
-				result = 1;
-			else
-				result = 0;
-		}
-
-		cout << result << endl;
+		else if (d == cond1 || d == cond2)
+			cout << "1" << '\n';
+		else if (cond1 < d && d < cond2)
+			cout << "2" << '\n';
+		else
+			cout << "0" << '\n';
 	}
 }
