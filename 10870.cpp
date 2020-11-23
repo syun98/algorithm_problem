d5f29049654c3de6da54d898ccf30a20853631c2
chocoo98 <chocoo98@naver.com>
2020-11-24 00:07:30 +0900

    Update 10870.cpp

--- 10870.cpp
+++ 10870.cpp
@@ -1,19 +1,21 @@
+//blog : https://codesyun.tistory.com/74
 #include <iostream>
 using namespace std;
 
-int fibonacci(int n) {
-	if (n == 0)
+int fibo(int n) {
+	if (n == 0) {
 		return 0;
-	else if (n == 1)
+	}
+	else if (n == 1) {
 		return 1;
+	}
 	else {
-		return fibonacci(n - 1) + fibonacci(n - 2);
+		return fibo(n - 1) + fibo(n - 2);
 	}
 }
 
 int main() {
 	int n;
 	cin >> n;
-	
-	cout << fibonacci(n) << endl;
+	cout << fibo(n) << '\n';
 }
