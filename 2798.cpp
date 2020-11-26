7092a5902147cd9e4654f908a9684eb57fbf86b2
chocoo98 <chocoo98@naver.com>
2020-11-27 05:16:01 +0900

    Update 2798.cpp

--- 2798.cpp
+++ 2798.cpp
@@ -1,32 +1,25 @@
+//blog : https://codesyun.tistory.com/80
 #include <iostream>
-#include <algorithm>
 using namespace std;
 
 int main() {
-	int n, m;
-	int card[100] = { 0, };
-	int sum = 0;
-	int res = 0;
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
+	int n, m, result = 0;
+	int arr[101] = {};
 	cin >> n >> m;
-
-	for (int i = 0; i < n; i++) {
-		cin >> card[i];
-	}
-
-	int min = m;
+	for (int i = 0; i < n; i++)
+		cin >> arr[i];
 
 	for (int i = 0; i < n; i++) {
 		for (int j = i + 1; j < n; j++) {
 			for (int k = j + 1; k < n; k++) {
-				sum = card[i] + card[j] + card[k];
-				if (m - sum < min&&sum <= m) {
-					min = m - sum;
-					res = sum;
-				}
+				if (arr[i] + arr[j] + arr[k] <= m && arr[i] + arr[j] + arr[k] > result)
+					result = arr[i] + arr[j] + arr[k];
 			}
 		}
 	}
-
-	cout << res << endl;
+	cout << result << '\n';
 }
