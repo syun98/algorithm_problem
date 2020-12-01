943082c2aa23190a5c217268aa1df01b40736d3a
chocoo98 <chocoo98@naver.com>
2020-12-01 23:41:26 +0900

    Update 1436.cpp

--- 1436.cpp
+++ 1436.cpp
@@ -1,20 +1,25 @@
+//blog : https://codesyun.tistory.com/84
 #include <iostream>
 #include <string>
 using namespace std;
 
 int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
 	int n;
-	int res = 665;
+	int result = 665;
 	string temp;
 	cin >> n;
 
 	for (int i = 0; i < n; i++) {
 		while (1) {
-			res++;
-			temp = to_string(res);
+			result++;
+			temp = to_string(result);
 			if (temp.find("666") != -1)
 				break;
 		}
 	}
-	cout << res;
+	cout << result << '\n';
 }
