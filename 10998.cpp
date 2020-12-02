057d744b78e963c7bf7f89cf514b3a15c94833c0
chocoo98 <chocoo98@naver.com>
2020-12-03 05:32:35 +0900

    Make Folder

--- 10998.cpp
+++
@@ -1,9 +0,0 @@
-//blog : https://codesyun.tistory.com/9
-#include <iostream>
-using namespace std;
-
-int main() {
-	int a, b;
-	cin >> a >> b;
-	cout << a * b << endl;
-}
---
+++ solved_problem/10998.cpp
@@ -0,0 +1,9 @@
+//blog : https://codesyun.tistory.com/9
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+	cout << a * b << endl;
+}
