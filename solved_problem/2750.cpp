d4ed1165b92ac8285d992331ada5bdd6d433f784
chocoo98 <chocoo98@naver.com>
2020-12-03 22:36:02 +0900

    Update 2750.cpp

--- solved_problem/2750.cpp
+++ solved_problem/2750.cpp
@@ -1,21 +1,27 @@
+//blog : https://codesyun.tistory.com/86
 #include <iostream>
-#include <vector>
 #include <algorithm>
 using namespace std;
 
 int main() {
-	int n, temp;
-	vector<int> v;
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n;
+	int arr[1001];
 	cin >> n;
+	for (int i = 0; i < n; i++)
+		cin >> arr[i];
 
-	for (int i = 0; i < n; i++) {
-		cin >> temp;
-		v.push_back(temp);
+	int temp;
+	for (int i = 1; i < n; i++) {
+		int *p = min_element(arr + i-1, arr + n);
+		temp = *p;
+		*p = arr[i - 1];
+		arr[i - 1] = temp;
 	}
 
-	sort(v.begin(), v.end());
-
-	for (int i = 0; i < n; i++) {
-		printf("%d\n", v[i]);
-	}
+	for (int i = 0; i < n; i++)
+		cout << arr[i] << '\n';
 }
