0efcbd7771f18865233e2efc5b2e65ae9a233652
chocoo98 <chocoo98@naver.com>
2020-12-04 04:58:16 +0900

    Update 2751.cpp

--- solved_problem/2751.cpp
+++ solved_problem/2751.cpp
@@ -1,22 +1,21 @@
+//blog : https://codesyun.tistory.com/85
 #include <iostream>
-#include <vector>
 #include <algorithm>
 using namespace std;
 
 int main() {
-	int n, temp;
-	vector<int> v;
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n;
+	int arr[1000001];
 	cin >> n;
+	for (int i = 0; i < n; i++)
+		cin >> arr[i];
 
-	for (int i = 0; i < n; i++) {
-		cin >> temp;
-		v.push_back(temp);
-	}
+	sort(arr, arr + n);
 
-	sort(v.begin(), v.end());
-	
-	for (int i = 0; i < n; i++) {
-		//time out when used cout
-		printf("%d\n", v[i]);
-	}
+	for (int i = 0; i < n; i++)
+		cout << arr[i] << '\n';
 }
